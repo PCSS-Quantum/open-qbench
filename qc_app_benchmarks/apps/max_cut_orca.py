decdb02cba6cd8f11acc342d98013ea3cc63d1d3
Sofiia Ivanko <sivanko@man.poznan.pl>
2024-03-08 13:56:04 +0100

    max_cut_orca benchmark

---
+++ qc_app_benchmarks/apps/max_cut_orca.py
@@ -0,0 +1,9 @@
+def max_cut_thetas_6_edges(return_graph=False):
+    if return_graph:
+        return "Graph with 6 nodes and 10 edges"
+    return [-3.0492, -0.1812, -0.7512, -2.1761,  0.2920]
+
+def max_cut_thetas_7_edges(return_graph=False):
+    if return_graph:
+        return "Graph with 7 nodes and 16 edges"
+    return [-1.5334,  0.0372,  0.8819, -1.9504,  0.6715,  2.6831]
