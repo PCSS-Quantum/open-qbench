0a8c2e2d98bdd6f84dbab0b45ae7a83f499a2d03
Lukaszgrodzki23 <115477342+Lukaszgrodzki23@users.noreply.github.com>
2024-06-26 14:35:22 +0200

    added function that generates all possible outputs analitically for a given input

--- qc_app_benchmarks/fidelities.py
+++ qc_app_benchmarks/fidelities.py
@@ -7,7 +7,7 @@ import itertools
 import math
 import numpy as np
 from ptseries.tbi import create_tbi
-
+from ptseries.tbi.tbi_multi_loop import _get_unitary_tbi_numerical
 
 
 def normalized_fidelity(dist_ideal: dict, dist_backend: dict) -> float:
@@ -98,9 +98,84 @@ def generate_all_possible_outputs_orca(input_state):
             all_possible_outputs.append(el)
     return all_possible_outputs
 
+def per(mtx, column, selected, prod, output=False):
+    
+    if column == mtx.shape[1]:
+        if output:
+            print(selected, prod)
+        return prod
+    else:
+        result = 0
+        for row in range(mtx.shape[0]):
+            if not row in selected:
+                result = result \
+                + per(mtx, column+1, selected+[row], prod*mtx[row,column])
+        return result
+
+def compute_permanent(mat):
+    return per(mat, 0, [], 1)
+
+def construct_unitary(bm_parameters):
+    def beamsplitter_matrix(theta, n, i, j):
+        U = np.eye(n, dtype=complex)
+        cos_theta = np.cos(theta)
+        sin_theta = np.sin(theta)
+        
+        U[i, i] = cos_theta
+        U[i, j] = -sin_theta
+        U[j, i] = sin_theta
+        U[j, j] = cos_theta
+        
+        return U
+    def apply_sequential_beam_splitters(thetas):
+        n = len(thetas) + 1
+        U = np.eye(n, dtype=complex)
+        for k, theta in enumerate(thetas):
+            if k + 1 < n:
+                BS = beamsplitter_matrix(theta, n, k, k + 1)
+                U = BS @ U
+        return U
+    
+    return  apply_sequential_beam_splitters(bm_parameters)
+
+def generate_submatrix(U, input, output):
+    columns = [j for j, t in enumerate(output) for _ in range(t)]
+    UT = U[:, columns]
+
+    rows = [i for i, s in enumerate(input) for _ in range(s)]
+    UST = UT[rows, :]
+    return UST
+
+def output_probabilities(input_bitstring, U):
+    possible_outputs = generate_all_possible_outputs_orca(input_bitstring)
+    probabilities = {}
+    
+    for output_indices in possible_outputs:
+        U_submatrix = generate_submatrix(U, input_bitstring, output_indices)
+        PQT = np.abs(compute_permanent(U_submatrix)) **2
+        probabilities[tuple(output_indices)] = PQT/(np.prod([math.factorial(s) for s in input_bitstring]) * np.prod([math.factorial(t) for t in output_indices]))
+        
+    total_prob = sum(probabilities.values())
+    for key in probabilities:
+        probabilities[key] /= total_prob
+    
+    return probabilities
+
+def generate_analitically(input_bitstring):
+    input_bitstring = input_bitstring[::-1]
+    theta_list = [np.pi/4] * (len(input_bitstring) - 1)
+    modes = len(input_bitstring)
+    U = construct_unitary(theta_list)
+    probabilities = output_probabilities(input_bitstring, U)
+    return sorted([(key[::-1]) for key, value in probabilities.items() if value > 1e-30 and sum(key) == sum(input_string)])
+
 if __name__ == "__main__":
-    ans1 = generate_all_samples_orca((1, 0, 1))
-    ans2 = generate_all_possible_outputs_orca((1, 0, 1))
+    input_string = (1,1,2)
+    ans0 = generate_analitically(input_string)
+    ans1 = generate_all_samples_orca(input_string)
+    ans2 = generate_all_possible_outputs_orca(input_string)
+    print(ans0)
+    print(len(ans0))
     print(ans1)
     print(len(ans1))
     print(ans2)
