5aa6f504e104a7dd34f38e6cdc6b0657591f1a7a
Konrad Wojciechowski <kwojciechowski@man.poznan.pl>
2025-02-21 14:42:15 +0100

    define BosonicSampler interface

--- qc_app_benchmarks/sampler/bosonic_sampler.py
+++ qc_app_benchmarks/sampler/bosonic_sampler.py
@@ -1,28 +1,24 @@
-from .base_sampler import BaseBenchmarkSampler, SamplerResult
+from abc import abstractmethod
+from typing import Iterable, Tuple
+
+from qiskit.primitives import BasePrimitiveJob, BaseSamplerV2
+
+from ..photonics import PhotonicCircuit
 
 
-class BosonicSampler(BaseBenchmarkSampler):
-    def __init__(self, sampler, default_samples: int = 100):
-        super().__init__(default_samples=default_samples)
-        self.sampler = sampler
+class BosonicSampler(BaseSamplerV2):
+    @abstractmethod
+    def run(
+        self, pubs: Iterable[Tuple[PhotonicCircuit, float]], *, shots: int | None = None
+    ) -> BasePrimitiveJob:
+        """This class differs from BaseSamplerV2 only in types of pubs and the returned job object.
 
-    def run(self, sampler_input, num_samples=None) -> SamplerResult:
-        input_state = sampler_input[0]
-        theta_list = sampler_input[1]
-        n_samples = num_samples or self.default_samples
-        return self.sampler.sample(
-            input_state,
-            theta_list,
-            n_samples=n_samples,
-            output_format="dict",
-            n_tiling=1,
-        )
+        Args:
+            pubs (Iterable[Tuple[PhotonicCircuit, float]]): An iterable of pubs containing PhotonicCircuits
+            (should be extended to more general pubs)
+            shots (int | None, optional): Number of collected samples.
 
-
-# if __name__ == "__main__":
-#     from ptseries.tbi import TBISingleLoop
-
-#     orca_sampler = BosonicSampler(TBISingleLoop(), default_samples=10)
-#     input_st = (1, 1, 1, 1, 1, 1, 1)
-#     thetas = [-1.5334, 0.0372, 0.8819, -1.9504, 0.6715, 2.6831]
-#     print(orca_sampler.run((input_st, thetas)))
+        Returns:
+            Job object of BosonicSampler's result
+        """
+        pass
