53317ec01a2948ad9c3d51ce6c5c5e14300f9c97
Anatol Kaczmarek <anatolk@man.poznan.pl>
2025-03-04 21:57:20 +0100

    tweaks to tests

--- test/test_orca_sampler.py
+++ test/test_orca_sampler.py
@@ -4,6 +4,47 @@ from qc_app_benchmarks.photonics import PhotonicCircuit, PhotonicRegister
 from qc_app_benchmarks.sampler.orca_sampler import OrcaSampler
 
 
+def test_conversion1():
+    ph_circuit = PhotonicCircuit(PhotonicRegister(4))
+    ph_circuit.input_state = [1, 1, 1, 1]
+    ph_circuit.bs(np.pi/4, 0, 1)
+    ph_circuit.bs(np.pi/4, 1, 2)
+    ph_circuit.bs(np.pi/4, 0, 2)
+    ph_circuit.bs(np.pi/4, 1, 3)
+    ph_circuit.bs(np.pi/4, 0, 3)
+    _, new_thetas, loop_lengths = OrcaSampler()._extract_lengths((ph_circuit, [np.pi/4]*5))
+    assert loop_lengths == [1, 2, 3]
+    assert new_thetas == [np.pi/4, np.pi/4, 0, np.pi/4, np.pi/4, np.pi/4]
+
+
+def test_conversion2():
+    ph_circuit = PhotonicCircuit(PhotonicRegister(4))
+    ph_circuit.input_state = [1, 1, 1, 1]
+    ph_circuit.bs(np.pi/4, 0, 1)
+    ph_circuit.bs(np.pi/4, 0, 3)
+    ph_circuit.bs(np.pi/4, 2, 3)
+    ph_circuit.bs(np.pi/4, 1, 2)
+    ph_circuit.bs(np.pi/4, 0, 2)
+    ph_circuit.bs(np.pi/4, 1, 3)
+    _, new_thetas, loop_lengths = OrcaSampler()._extract_lengths((ph_circuit, [np.pi/4]*6))
+    assert loop_lengths == [1, 3, 1, 1, 2]
+    assert new_thetas == [np.pi/4, 0, 0, np.pi/4, 0, 0, np.pi/4, 0, np.pi/4, 0, np.pi/4, np.pi/4]
+
+
+def test_validation():
+    ph_circuit = PhotonicCircuit(PhotonicRegister(4))
+    ph_circuit.input_state = [1, 1, 1, 1]
+    ph_circuit.bs(np.pi/4, 0, 1)
+    ph_circuit.bs(np.pi/4, 1, 2)
+    ph_circuit.bs(np.pi/4, 2, 3)
+    ph_circuit.bs(np.pi/4, 0, 2)
+    ph_circuit.bs(np.pi/4, 1, 3)
+    ph_circuit.bs(np.pi/4, 0, 3)
+
+    with pytest.raises(TypeError):
+        _ = OrcaSampler().run([(ph_circuit, [np.pi/4]*5+[np.pi]), (ph_circuit, [np.pi/4]*6)])
+
+
 def test_sampler():
     # Valid circuit 1
     ph_circuit1 = PhotonicCircuit(PhotonicRegister(4))
@@ -30,32 +71,3 @@ def test_sampler():
     assert isinstance(job.result()[0], dict)
 
     assert isinstance(job.result()[1], dict)
-
-
-def test_vaildation1():
-    # Invalid Circuit
-    ph_circuit = PhotonicCircuit(PhotonicRegister(4))
-    ph_circuit.input_state = [1, 1, 1, 1]
-    ph_circuit.bs(np.pi/4, 0, 1)
-    ph_circuit.bs(np.pi/4, 1, 2)
-    ph_circuit.bs(np.pi/4, 0, 2)
-    ph_circuit.bs(np.pi/4, 1, 3)
-    ph_circuit.bs(np.pi/4, 0, 3)
-    _, new_thetas, loop_lengths = OrcaSampler()._extract_lengths((ph_circuit, [np.pi/4]*5))
-    assert loop_lengths == [1, 2, 3]
-    assert new_thetas == [np.pi/4, np.pi/4, 0, np.pi/4, np.pi/4, np.pi/4]
-
-
-def test_validation2():
-    # Invalid Circuit
-    ph_circuit = PhotonicCircuit(PhotonicRegister(4))
-    ph_circuit.input_state = [1, 1, 1, 1]
-    ph_circuit.bs(np.pi/4, 0, 1)
-    ph_circuit.bs(np.pi/4, 0, 3)
-    ph_circuit.bs(np.pi/4, 2, 3)
-    ph_circuit.bs(np.pi/4, 1, 2)
-    ph_circuit.bs(np.pi/4, 0, 2)
-    ph_circuit.bs(np.pi/4, 1, 3)
-    _, new_thetas, loop_lengths = OrcaSampler()._extract_lengths((ph_circuit, [np.pi/4]*6))
-    assert loop_lengths == [1, 3, 1, 1, 2]
-    assert new_thetas == [np.pi/4, 0, 0, np.pi/4, 0, 0, np.pi/4, 0, np.pi/4, 0, np.pi/4, np.pi/4]
