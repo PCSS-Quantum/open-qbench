990529ff44204f0cd21fe199b93bc7e01f8ac39c
Jakub Jagla <jakub.jagla01@gmail.com>
2025-08-21 10:33:23 +0200

    utils v2 sampler fixup

--- open_qbench/utils.py
+++ open_qbench/utils.py
@@ -1,9 +1,9 @@
 import json
 
 from qiskit_aer.noise import NoiseModel
-from qiskit_aer.primitives import Sampler as AerSampler
-from qiskit_ibm_runtime import Options, QiskitRuntimeService
-from qiskit_ibm_runtime import Sampler as RuntimeSampler
+from qiskit_aer.primitives import SamplerV2 as AerSampler
+from qiskit_ibm_runtime import QiskitRuntimeService
+from qiskit_ibm_runtime import SamplerV2 as RuntimeSampler
 from qiskit_ibm_runtime.fake_provider.fake_backend import (
     FakeBackendV2,
 )
@@ -16,7 +16,7 @@ def get_fake_backend_sampler(
     shots: int | None = None,
     seed: int | None = None,
 ) -> AerSampler:
-    """Create a sampler from qiskit_aer based on a noise model supplied by a Qiskit fake backend.
+    """Creates a sampler from qiskit_aer based on a noise model supplied by a Qiskit fake backend
 
     Args:
         fake_backend (FakeBackendV2): an object representing a Qiskit fake backend
@@ -26,19 +26,24 @@ def get_fake_backend_sampler(
 
     Returns:
         AerSampler: _description_
-
     """
     coupling_map = fake_backend.coupling_map
     noise_model = NoiseModel.from_backend(fake_backend)
 
     backend_sampler = AerSampler(
-        backend_options={
-            "method": "density_matrix",
-            "coupling_map": coupling_map,
-            "noise_model": noise_model,
-        },
-        run_options={"seed": seed, "shots": shots},
-        transpile_options={"seed_transpiler": seed},
+        options={
+            "backend_options": {
+                "method": "density_matrix",
+                "coupling_map": coupling_map,
+                "noise_model": noise_model,
+            },
+            "run_options": {"seed": seed, "shots": shots},
+            # TODO: find another way to parametrize transpilation
+            # 'transpile_options':
+            #     {
+            #         "seed_transpiler": seed
+            #     },
+        }
     )
     return backend_sampler
 
@@ -46,10 +51,14 @@ def get_fake_backend_sampler(
 def get_ibm_backend_sampler(name: str, shots):
     service = QiskitRuntimeService(channel="ibm_quantum")
     backend = service.backend(name)
-    options = Options(optimization_level=3, resilience_level=0)
-    options.execution.shots = shots  # type: ignore
-    ibm_sampler = RuntimeSampler(backend, options=options)
-
+    # TODO: no transpilation options available for Sampler v2
+    # options = Options(optimization_level=3, resilience_level=0)
+    ibm_sampler = RuntimeSampler(
+        backend,
+        options={
+            "default_shots": shots,
+        },
+    )
     return ibm_sampler
 
 
@@ -61,7 +70,6 @@ def calculate_from_file(file: str) -> float:
 
     Returns:
         float: Normalized fidelity of the provided distributions
-
     """
     with open(file, "rb") as f:
         result = json.load(f)
